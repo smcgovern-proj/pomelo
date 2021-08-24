0582d794756ead86b7b81c9677b4618f42184b9d
Sean <smcgovern135@gmail.com>
2021-08-24 10:55:29 -0400

    initialized Timer component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,21 @@
 <script>
+  import Timer from './Timer.svelte';
+  //boolean state to keep track of whether timer is currently running
+  let counting = false;
 
+  //click handler for main button
+  const startHandler = (e) => {
+    e.preventDefault();
+    counting = !counting;
+  };
 </script>
 
 <main>
   <div class="clock-container">
-    <div class="clock">time here</div>
-    <button class="start"></button>
+    <Timer />
+    <button class="start-button" on:click={startHandler}>
+      {counting ? "stop" : "start"}
+    </button>
   </div>
 </main>
 <div class="footer">somebody hire me please</div>
@@ -21,5 +31,24 @@
 
   .clock-container {
     border: 1px solid black; 
+    display: flex;
+    flex-flow: column nowrap;
+    width: 80vh;
+    height: 60vh;
+    align-items: center;
+    justify-content: space-evenly;
+  }
+  .footer {
+    margin-bottom: 2rem;
+    text-align: center;
+  }
+
+  .start-button {
+    font-family: var(--font-display);
+    font-size: 2rem;
+    border: 2px solid black;
+    border-radius: .5rem;
+    width: 60%;
+    height: 20%;
   }
 </style>
---
+++ src/Timer.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<div class="outer">
+  timer here
+</div>
+
+<style>
+  .outer {
+    border: 1px solid black;
+    width: 100%;
+    height: 60%;
+  }
+</style>
